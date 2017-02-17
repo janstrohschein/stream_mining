da618d457d01a9201756e958247825139910a92d
Jan Strohschein <strohschein@gmx.net>
2017-02-17 22:32:13 +0100

    combined classes, keywords and elements in config

--- src/get_tweets_with_listener.py
+++ src/get_tweets_with_listener.py
@@ -7,35 +7,39 @@ from tweepy.streaming import StreamListener
 from tweepy import OAuthHandler
 from tweepy import Stream
 
-class TwitterMiner:
-    def __init__(self):
-        self.config = {}
-        self.read_config()
-
-    def read_config(self):
-            config = ConfigParser()
-            config.read("C:/Users/Jan/Documents/GitHub/stream_mining/src/twitter.ini", encoding='utf-8')
-
-            try:
-                sections = config.sections()
-                if len(sections) == 0:
-                    sys.exit()
-                for section in sections:
-
-                    self.config[section] = {}
-                    for option in config.options(section):
-                        self.config[section][option] = config.get(section, option)
-            except:
-                print("The config file path is not valid")
-                sys.exit()
-
 
 #This is a basic listener that just prints received tweets to stdout.
 class StdOutListener(StreamListener):
 
+    def __init__(self):
+        super(StdOutListener, self).__init__()
+        self.config = {}
+        self.read_config()
+
+    def read_config(self):
+        config = ConfigParser()
+        config.read("C:/Users/Jan/Documents/GitHub/stream_mining/src/twitter.ini", encoding='utf-8')
+
+        try:
+            sections = config.sections()
+            if len(sections) == 0:
+                sys.exit()
+            for section in sections:
+
+                self.config[section] = {}
+                for option in config.options(section):
+                    self.config[section][option] = config.get(section, option).split(',')
+        except:
+            print("The config file path is not valid")
+            sys.exit()
+
     def on_data(self, data):
         tweet = self.read_json(data)
-        print(tweet.get('text', 'No text'))
+        if 'user' in self.config['Extract']['elements'] and 'user' in tweet:
+            print(tweet['user'].get('name', 'No Name'), ':')
+
+        if 'text' in self.config['Extract']['elements']:
+            print(tweet.get('text', 'No text'))
         return True
 
     def on_error(self, status):
@@ -47,13 +51,11 @@ class StdOutListener(StreamListener):
 
 
 if __name__ == '__main__':
-
-    m = TwitterMiner()
     #This handles Twitter authetification and the connection to Twitter Streaming API
     l = StdOutListener()
-    auth = OAuthHandler(m.config['Twitter']['consumer_key'], m.config['Twitter']['consumer_secret'])
-    auth.set_access_token(m.config['Twitter']['access_token'], m.config['Twitter']['access_token_secret'])
+    auth = OAuthHandler(*l.config['Twitter']['consumer_key'], *l.config['Twitter']['consumer_secret'])
+    auth.set_access_token(*l.config['Twitter']['access_token'], *l.config['Twitter']['access_token_secret'])
     stream = Stream(auth, l)
 
     #This line filter Twitter Streams to capture data by the keywords: 'python', 'javascript', 'ruby'
-    stream.filter(track=['Trump'])
+    stream.filter(track=l.config['Extract']['keywords'])

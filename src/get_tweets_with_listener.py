dd514b3e6be8d81b00abcabaf2aba89c02c85651
Jan Strohschein <strohschein@gmx.net>
2017-02-18 11:07:14 +0100

    csv output in utf8
    reads storage path from config file

--- src/get_tweets_with_listener.py
+++ src/get_tweets_with_listener.py
@@ -1,6 +1,9 @@
 from configparser import ConfigParser
 import sys
 import json
+import csv
+import codecs
+import re
 
 #Import the necessary methods from tweepy library
 from tweepy.streaming import StreamListener
@@ -33,13 +36,36 @@ class StdOutListener(StreamListener):
             print("The config file path is not valid")
             sys.exit()
 
+    def open_csv_writer(self):
+        # Open/Create a file to append data
+        #self.csvFile = open('result.csv', 'a')
+        if 'csv' in self.config['Store']:
+            self.csvFile = codecs.open(self.config['Store']['csv'][0], 'a', encoding="utf-8")
+            print('Store data in ' + self.config['Store']['csv'][0])
+        else:
+            self.csvFile = codecs.open('result.csv', 'a', encoding="utf-8")
+            print('Store data in standard file result.csv')
+        #Use csv Writer
+        self.csvWriter = csv.writer(self.csvFile)
+
     def on_data(self, data):
         tweet = self.read_json(data)
+        tweet_extract = []
         if 'user' in self.config['Extract']['elements'] and 'user' in tweet:
-            print(tweet['user'].get('name', 'No Name'), ':')
+            tweet_extract.append(tweet['user'].get('name', 'No Name') + ': ')
 
         if 'text' in self.config['Extract']['elements']:
-            print(tweet.get('text', 'No text'))
+            tweet_extract.append(tweet.get('text', 'No text'))
+
+        tweet_extract = [re.sub('\s+',' ',item) for item in tweet_extract]
+
+        if 'csv' in self.config['Store']:
+            self.csvWriter.writerow(tweet_extract)
+            # writes to file before self.csvFile.close()
+            self.csvFile.flush()
+
+        print(*tweet_extract)
+
         return True
 
     def on_error(self, status):
@@ -49,7 +75,6 @@ class StdOutListener(StreamListener):
         return json.loads(data, 'utf-8')
 
 
-
 if __name__ == '__main__':
     #This handles Twitter authetification and the connection to Twitter Streaming API
     l = StdOutListener()
@@ -57,5 +82,10 @@ if __name__ == '__main__':
     auth.set_access_token(*l.config['Twitter']['access_token'], *l.config['Twitter']['access_token_secret'])
     stream = Stream(auth, l)
 
+    # workaround for windows console missing utf-8
+    sys.stdout = codecs.getwriter('utf8')(sys.stdout.buffer)
+
+    l.open_csv_writer()
     #This line filter Twitter Streams to capture data by the keywords: 'python', 'javascript', 'ruby'
     stream.filter(track=l.config['Extract']['keywords'])
+    l.csvFile.close()
